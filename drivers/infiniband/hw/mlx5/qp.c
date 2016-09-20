cb9cd389513e8826596c6ba08a7c9a4bc01d281f
Maor Gottlieb <maorg@mellanox.com>
2016-09-20 13:59:49 +0300

    IB/mlx5: Add receive inline EXP
    Add support for receive inline new API.
    
    Issue: 865147
    Change-Id: I9055e547bc48d8bf8002b0c445773a5d6c1f32d3
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -1975,6 +1975,9 @@ static int create_qp_common(struct mlx5_ib_dev *dev, struct ib_pd *pd,
 	       &qp->raw_packet_qp.rq.base :
 	       &qp->trans_qp.base;
 
+	if (is_exp && mlx5_ib_exp_max_inl_recv(init_attr))
+		qp->scat_cqe = 1;
+
 	qp->has_rq = qp_has_rq(init_attr);
 	err = set_rq_size(dev, &init_attr->cap, qp->has_rq,
 			  qp, (pd && pd->uobject) ? pucmd : NULL);
