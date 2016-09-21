228b7f2a7b9a7b049e3a31d26ca964a3a7639bfb
Mark Bloch <markb@mellanox.com>
2016-09-21 11:43:51 +0000

    IB/core: Restrict Physical Address MR usage
    Physical Address MR is allowed only for users with the capability
    SYS_RAW_IO, which means they can write directly to the physical
    memory.
    
    Make sure PA MR is used with the following allowed flags:
    IB_ACCESS_LOCAL_WRITE
    IB_ACCESS_REMOTE_WRITE
    IB_ACCESS_REMOTE_READ
    IB_ACCESS_REMOTE_ATOMIC
    
    Issue: 866494
    Change-Id: I1c1e73200b27af0aef2709fa0ea94a53a543b3b6
    Signed-off-by: Mark Bloch <markb@mellanox.com>
    Signed-off-by: Alex Vesker <valex@mellanox.com>

--- include/rdma/ib_verbs_exp_def.h
+++ include/rdma/ib_verbs_exp_def.h
@@ -8,4 +8,12 @@ enum ib_qpg_type {
 	IB_QPG_CHILD_TX = (1<<2)
 };
 
+enum ib_exp_start_values {
+	IB_EXP_ACCESS_FLAGS_SHIFT = 0x0F
+};
+
+enum ib_exp_access_flags {
+	/* Initial values are non-exp defined as part of  ib_access_flags */
+	IB_EXP_ACCESS_PHYSICAL_ADDR	    = (1 << (16 + IB_EXP_ACCESS_FLAGS_SHIFT)),
+};
 #endif
