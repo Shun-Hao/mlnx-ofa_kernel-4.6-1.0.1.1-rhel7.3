682c04146282c8fa2f9e96adb65eef14998f327e
Maor Gottlieb <maorg@mellanox.com>
2016-10-27 17:46:03 +0300

    IB/mlx5: Add EXP receive Work Queue verbs
    Issue: 865165
    Change-Id: I6e43279e3863d2f744303bc0c0bef2af051527f0
    Signed-off-by: Maor Gottlieb <maorg@mellanox.com>
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- drivers/infiniband/hw/mlx5/main.c
+++ drivers/infiniband/hw/mlx5/main.c
@@ -5986,6 +5986,11 @@ static int mlx5_ib_stage_common_roce_init(struct mlx5_ib_dev *dev)
 
 	port_num = mlx5_core_native_port_num(dev->mdev) - 1;
 
+	dev->ib_dev.uverbs_exp_cmd_mask |=
+		(1ull << IB_USER_VERBS_EXP_CMD_CREATE_WQ) |
+		(1ull << IB_USER_VERBS_EXP_CMD_MODIFY_WQ) |
+		(1ull << IB_USER_VERBS_EXP_CMD_DESTROY_WQ);
+
 	return mlx5_add_netdev_notifier(dev, port_num);
 }
 
