41ab18a29d7b959e1bd6b642e7869f2f7ccfacba
Mark Bloch <markb@mellanox.com>
2016-12-19 10:57:02 +0000

    IB/uverbs: Add disable raw qp module param
    Issue: 928045
    Change-Id: I1c106d0605879b2874f7c99cb758066b8eb314ca
    Signed-off-by: Mark Bloch <markb@mellanox.com>

--- drivers/infiniband/core/uverbs_cmd.c
+++ drivers/infiniband/core/uverbs_cmd.c
@@ -48,6 +48,12 @@
 #include "uverbs_exp.h"
 #include "core_priv.h"
 
+static int disable_raw_qp_enforcement;
+module_param_named(disable_raw_qp_enforcement, disable_raw_qp_enforcement, int,
+		   0444);
+MODULE_PARM_DESC(disable_raw_qp_enforcement, "Disable RAW QP enforcement for "
+		 "being opened by root (default: 0)");
+
 static struct ib_uverbs_completion_event_file *
 _ib_uverbs_lookup_comp_file(s32 fd, struct ib_uverbs_file *ufile)
 {
@@ -1358,7 +1364,8 @@ static int create_qp(struct ib_uverbs_file *file,
 	bool has_sq = true;
 	struct ib_device *ib_dev;
 
-	if (cmd->qp_type == IB_QPT_RAW_PACKET && !capable(CAP_NET_RAW))
+	if (!disable_raw_qp_enforcement &&
+	    cmd->qp_type == IB_QPT_RAW_PACKET && !capable(CAP_NET_RAW))
 		return -EPERM;
 
 	obj = (struct ib_uqp_object *)uobj_alloc(UVERBS_OBJECT_QP, file,
@@ -3516,7 +3523,9 @@ int ib_uverbs_create_flow_common(struct ib_uverbs_file *file,
 	if (cmd.comp_mask)
 		return -EINVAL;
 
-	if (!capable(CAP_NET_RAW))
+	if ((cmd.flow_attr.type == IB_FLOW_ATTR_SNIFFER &&
+	     !capable(CAP_NET_ADMIN)) ||
+	    (!capable(CAP_NET_RAW) && !disable_raw_qp_enforcement))
 		return -EPERM;
 
 	if (cmd.flow_attr.flags >= IB_FLOW_ATTR_FLAGS_RESERVED)
