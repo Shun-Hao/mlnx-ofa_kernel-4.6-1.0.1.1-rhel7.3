d07ab3a222bb8675a6ecf257f408080e91ef4b8c
Max Gurtovoy <maxg@mellanox.com>
2017-04-03 11:50:50 +0300

    IB/core: Add new SRQ type IB_EXP_SRQT_NVMF
    This patch adds new SRQ type - IB_EXP_SRQT_NVMF. The new SRQ type
    supports I/O path offload for NVMEoF target.
    The offload is implemented as peer-to-peer over PCI communication for
    supported hardware. In that case, an incoming NVMEoF command capsules
    would be directly posted to the NVMe SSD device without any CPU interference.
    For creating this new SRQ type, one must supply the following
    parameters to HCA configuration:
     - offload type - e.g. READ/WRITE/READ_WRITE
     - maximal namespaces supported by offload context
     - offloaded_capsules_count for statistics
     - cmd size for inline data
     - data offset in command capsule
     - memory page size
     - staging buffer description for staging data between transactions
     - supported queue size
    
    Issue: 920501
    
    Change-Id: I02c4ce058739ae8c9fcd18abfc680391b5806a21
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- include/rdma/ib_verbs_exp_def.h
+++ include/rdma/ib_verbs_exp_def.h
@@ -17,6 +17,28 @@ enum ib_exp_access_flags {
 	IB_EXP_ACCESS_PHYSICAL_ADDR	    = (1 << (16 + IB_EXP_ACCESS_FLAGS_SHIFT)),
 };
 
+enum ib_nvmf_offload_type {
+	IB_NVMF_WRITE_OFFLOAD		 = (1ULL << 0),
+	IB_NVMF_READ_OFFLOAD		 = (1ULL << 1),
+	IB_NVMF_READ_WRITE_OFFLOAD	 = (1ULL << 2),
+	IB_NVMF_READ_WRITE_FLUSH_OFFLOAD = (1ULL << 3),
+};
+
+struct ib_nvmf_init_data {
+	enum ib_nvmf_offload_type	type;
+	u8				log_max_namespace;
+	u32				offloaded_capsules_count;
+	u32				cmd_size;
+	u8				data_offset;
+	u8				log_max_io_size;
+	u8				nvme_memory_log_page_size;
+	u8				staging_buffer_log_page_size;
+	u16				staging_buffer_number_of_pages;
+	u32				staging_buffer_page_offset;
+	u16				nvme_queue_size;
+	u64				*staging_buffer_pas;
+};
+
 struct ib_odp_statistics {
 #ifdef CONFIG_INFINIBAND_ON_DEMAND_PAGING
 
