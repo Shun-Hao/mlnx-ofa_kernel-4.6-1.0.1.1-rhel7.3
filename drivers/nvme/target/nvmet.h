94f599c852ff1203bf07fb46c9caaee5a6b1e01b
Max Gurtovoy <maxg@mellanox.com>
2017-04-23 16:26:23 +0000

    nvmet: Add create/destroy_offload_ctrl for transport ops
    For transports that are capable to offload IO path, we need
    to create/destroy additional SW/HW context's.
    Also add install_offload_queue for transport queue initialization
    process.
    
    Issue: 920501
    
    Change-Id: Ifc298718337171acdd04d89b0f87b0ec58561ac6
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- drivers/nvme/target/nvmet.h
+++ drivers/nvme/target/nvmet.h
@@ -274,6 +274,9 @@ struct nvmet_fabrics_ops {
 	void (*disc_traddr)(struct nvmet_req *req,
 			struct nvmet_port *port, char *traddr);
 	bool (*peer_to_peer_capable)(struct nvmet_port *port);
+	int (*install_offload_queue)(struct nvmet_ctrl *ctrl, u16 qid);
+	int (*create_offload_ctrl)(struct nvmet_ctrl *ctrl);
+	void (*destroy_offload_ctrl)(struct nvmet_ctrl *ctrl);
 };
 
 #define NVMET_MAX_INLINE_BIOVEC	8
