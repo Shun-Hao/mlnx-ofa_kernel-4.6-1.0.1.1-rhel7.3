790b55cfc30e472aefcdaa473f7b186ea8511251
Roi Dayan <roid@mellanox.com>
2017-10-15 11:39:00 +0300

    net/mlx5: Add lag function to get peer mdev
    The peer mdev is needed to get the peer PF handle in two ports HCA.
    
    Issue: 1159068
    Change-Id: I3bd732a40228bfdf13ef8dae86b2ed7d5e0154d5
    Signed-off-by: Roi Dayan <roid@mellanox.com>
    Signed-off-by: Shahar Klein <shahark@mellanox.com>
    Reviewed-by: Aviv Heller <avivh@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/lag.c
+++ drivers/net/ethernet/mellanox/mlx5/core/lag.c
@@ -797,3 +797,20 @@ unlock:
 	return ret;
 }
 EXPORT_SYMBOL(mlx5_lag_modify_cong_params);
+
+struct mlx5_core_dev *mlx5_lag_get_peer_mdev(struct mlx5_core_dev *dev)
+{
+	struct mlx5_core_dev *peer_dev = NULL;
+	struct mlx5_lag *ldev;
+
+	mutex_lock(&lag_mutex);
+	ldev = mlx5_lag_dev_get(dev);
+	if (!ldev)
+		goto unlock;
+
+	peer_dev = ldev->pf[0].dev == dev ? ldev->pf[1].dev : ldev->pf[0].dev;
+
+unlock:
+	mutex_unlock(&lag_mutex);
+	return peer_dev;
+}
