4e7d50361fe9e52cd84e0bb56e43f88ea221a024
Guy Levi <guyle@mellanox.com>
2018-01-03 17:53:21 +0200

    net/mlx5: Support Striding Receive WQ for SRQ
    Expose new attributes in mlx5_srq_attr to enable Striding Receive
    WQ feature on SRQ's context.
    
    Issue: 1250275
    Change-Id: Ie987a89524576a8c6150f045061b13e6dc94e43f
    Signed-off-by: Guy Levi <guyle@mellanox.com>

--- include/linux/mlx5/srq.h
+++ include/linux/mlx5/srq.h
@@ -40,6 +40,7 @@ enum {
 	MLX5_SRQ_FLAG_WQ_SIG = (1 << 1),
 	MLX5_SRQ_FLAG_RNDV   = (1 << 2),
 	MLX5_SRQ_FLAG_SET_DC_OP = 1 << 3,
+	MLX5_SRQ_FLAG_STRIDING_RECV_WQ = (1 << 4),
 };
 
 enum mlx5_nvmf_offload_type {
@@ -77,6 +78,11 @@ struct mlx5_dc_offload_params {
 	u32				ooo_caps;
 };
 
+struct mlx5_striding_recv_wq {
+	u8 log_wqe_num_of_strides;
+	u8 log_wqe_stride_size;
+};
+
 struct mlx5_srq_attr {
 	u32 type;
 	u32 flags;
@@ -100,6 +106,7 @@ struct mlx5_srq_attr {
 	u16 uid;
 	struct mlx5_nvmf_attr nvmf;
 	struct mlx5_dc_offload_params dc_op;
+	struct mlx5_striding_recv_wq	striding_recv_wq;
 };
 
 struct mlx5_core_dev;
