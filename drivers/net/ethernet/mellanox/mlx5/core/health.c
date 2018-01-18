253b267cd347aa39ddaeebdc8aa667341a935103
Alex Vesker <valex@mellanox.com>
2018-01-18 16:34:09 +0200

    net/mlx5: Adjust crdump collection after sensor check support
    Crdump collecting should be triggered in the following cases:
    -PCI state is not normal but FW PCI channel is open
    -FW syndrome with request for reset bit set
    
    issue: 1187431
    Change-Id: Ibe565babd530a3ff5cf0ddbcd8cac5fdd56bc8a3
    Signed-off-by: Alex Vesker <valex@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/health.c
+++ drivers/net/ethernet/mellanox/mlx5/core/health.c
@@ -136,19 +136,28 @@ static u32 mlx5_check_fatal_sensors(struct mlx5_core_dev *dev)
 
 void mlx5_enter_error_state(struct mlx5_core_dev *dev, bool force)
 {
+	u32 fatal_error, err;
+
 	mutex_lock(&dev->intf_state_mutex);
 	if (dev->state == MLX5_DEVICE_STATE_INTERNAL_ERROR)
 		goto unlock;
 
 	mlx5_core_err(dev, "start\n");
-	if (mlx5_check_fatal_sensors(dev) || force) {
+	fatal_error = mlx5_check_fatal_sensors(dev);
+
+	if (fatal_error || force) {
 		dev->state = MLX5_DEVICE_STATE_INTERNAL_ERROR;
 		mlx5_lock_and_flush_cmdif(dev);
 		mlx5_unlock_cmdif(dev);
-		mlx5_core_dbg(dev, "pci in bad state, crdump will not be collected.\n");
+	}
+
+	if (!force && fatal_error != MLX5_SENSOR_PCI_ERR &&
+			fatal_error != MLX5_SENSOR_PCI_COMM_ERR) {
+		err = mlx5_fill_cr_dump(dev);
+		if (err)
+			mlx5_core_err(dev, "Failed to collect crdump area err %d\n", err);
 	} else {
-		if (mlx5_fill_cr_dump(dev))
-			mlx5_core_err(dev, "Failed to collect crdump area.\n");
+		mlx5_core_dbg(dev, "pci in bad state, crdump will not be collected.\n");
 	}
 
 	mlx5_core_event(dev, MLX5_DEV_EVENT_SYS_ERROR, 1);
