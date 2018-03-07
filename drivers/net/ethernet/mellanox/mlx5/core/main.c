11c5e8e0dc420870b89405ce3cbdb7ae9a24896b
Huy Nguyen <huyn@mellanox.com>
2018-03-07 07:34:49 -0600

    net/mlx5: Check capi capability before calling cxllib_slot_is_supported
    Check the capi capability before calling
    cxllib_slot_is_supported to avoid the dmesg error printed out
    by the cxllib_slot_is_supported.
    	cxl: invalid capp unit id (phb_index: 4)
    
    Please squash this patch into CAPI implementation patch for OFED 4.4
    
    Issue: 1322887
    Squash: commit 20cc2c5bab01 ("mlx5: CAPI implementation")
    Change-Id: Ia5495555796c6a4174d1d2d63476b8923b7550df
    Signed-off-by: Huy Nguyen <huyn@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/main.c
+++ drivers/net/ethernet/mellanox/mlx5/core/main.c
@@ -1798,11 +1798,6 @@ static int capi_init(struct mlx5_core_dev *dev)
 	struct mlx5_core_capi *capi = &dev->capi;
 	int err;
 
-	if (!cxllib_slot_is_supported(dev->pdev, 0)) {
-		mlx5_core_dbg(dev, "slot does not NOT support CAPI\n");
-		return -ENOTSUPP;
-	}
-
 	err = mlx5_core_icmd_query_cap(dev, 0, &capi->icmd_caps);
 	if (err) {
 		mlx5_core_warn(dev, "failed to query icmd caps\n");
@@ -1813,6 +1808,11 @@ static int capi_init(struct mlx5_core_dev *dev)
 
 	if (!mlx5_capi_supported(dev)) {
 		mlx5_core_warn(dev, "capi is NOT enabled\n");
+		return -ENOTSUPP;
+	}
+
+	if (!cxllib_slot_is_supported(dev->pdev, 0)) {
+		mlx5_core_dbg(dev, "slot does not NOT support CAPI\n");
 		return -ENOTSUPP;
 	}
 
