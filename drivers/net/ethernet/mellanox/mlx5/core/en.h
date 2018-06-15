5b8c31d5a0af85a2bd374f49eeceb282eaec5381
Daniel Jurgens <danielj@mellanox.com>
2018-06-15 11:38:17 -0500

    net/mlx5: Don't create inner tirs if in ENCAP mode
    If the there is a flowtable for ENCAP mode inner tirs cannot be used.
    If the function isn't the eswitch manager this indicates it's using a
    smartnic and ENCAP mode is implied.
    
    Issue: 1415121
    Change-Id: I12cbd31d408a96bb535412d17bb39a1a66d86c46
    Signed-off-by: Daniel Jurgens <danielj@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en.h
@@ -51,6 +51,7 @@
 #include <net/switchdev.h>
 #include <net/xdp.h>
 #include <linux/net_dim.h>
+#include "eswitch.h"
 #include "wq.h"
 #include "mlx5_core.h"
 #include "en_stats.h"
@@ -991,7 +992,9 @@ static inline bool mlx5e_tunnel_inner_ft_supported(struct mlx5_core_dev *mdev)
 #else
 		 0) &&
 #endif
-		 MLX5_CAP_FLOWTABLE_NIC_RX(mdev, ft_field_support.inner_ip_version));
+		 MLX5_CAP_FLOWTABLE_NIC_RX(mdev, ft_field_support.inner_ip_version) &&
+		 (MLX5_ESWITCH_MANAGER(mdev) &&
+			mdev->priv.eswitch->offloads.encap == DEVLINK_ESWITCH_ENCAP_MODE_NONE));
 }
 
 #ifdef CONFIG_MLX5_INNER_RSS
