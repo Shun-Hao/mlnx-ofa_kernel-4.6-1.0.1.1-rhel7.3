65e7238aa3719262671fe331cb57a7b10f4bd059
Jianbo Liu <jianbol@mellanox.com>
2018-11-05 07:12:30 +0000

    net/mlx5: Fix the data structure for eswitch capabilities
    Issue: 1540370
    Change-Id: I2d006c38040a302309631a47fc37cba05799961c
    Signed-off-by: Jianbo Liu <jianbol@mellanox.com>

--- include/linux/mlx5/mlx5_ifc.h
+++ include/linux/mlx5/mlx5_ifc.h
@@ -685,10 +685,10 @@ struct mlx5_ifc_e_switch_cap_bits {
 	u8         vport_cvlan_insert_if_not_exist[0x1];
 	u8         vport_cvlan_insert_overwrite[0x1];
 	u8         reserved_at_5[0x1];
-	u8         merged_eswitch[0x1];
 	u8         vport_cvlan_insert_always[0x1];
 	u8         reserved_at_7[0x15];
 	u8         counter_eswitch_affinity[0x1];
+	u8         merged_eswitch[0x1];
 	u8         nic_vport_node_guid_modify[0x1];
 	u8         nic_vport_port_guid_modify[0x1];
 
