37d0734447258c07d4dc8027d5f65e1a853ddc9e
Yishai Hadas <yishaih@mellanox.com>
2019-02-26 11:56:30 +0200

    IB/mlx5: Allocate the per-port Q counter shared when DEVX is  supported
    The per-port Q counter is some kernel resource and as such may be used
    by few UID(s) upon DEVX usage.
    
    To enable using it for QP/RQ when DEVX context is used need to allocate
    it with a sharing mode indication to let firmware allows its usage.
    
    The UID = 0xffff was chosen to mark it.
    
    issue: 1656788
    Change-Id: I52f35266eeb84c3cb2eb47792b06aa8e981df866
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- drivers/infiniband/hw/mlx5/cmd.h
+++ drivers/infiniband/hw/mlx5/cmd.h
@@ -63,4 +63,6 @@ int mlx5_cmd_alloc_sw_icm(struct mlx5_dm_mgr *dm_mgr, u64 *addr,
 			  u64 length, u32 *obj_id, int type);
 int mlx5_cmd_dealloc_sw_icm(struct mlx5_dm_mgr *dm_mgr, u64 addr,
 			    u64 length, u32 obj_id, int type);
+int mlx5_cmd_alloc_q_counter(struct mlx5_core_dev *dev, u16 *counter_id,
+			     u16 uid);
 #endif /* MLX5_IB_CMD_H */
