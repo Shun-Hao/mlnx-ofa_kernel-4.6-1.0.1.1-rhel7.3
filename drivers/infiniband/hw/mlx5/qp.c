c4cb502163b5336fdeddc2f41d88469f128eb0aa
Yishai Hadas <yishaih@mellanox.com>
2019-03-12 10:25:57 +0200

    IB/mlx5: Use mlx5 core to create/destroy a DEVX DCT
    To prevent a hardware memory leak when a DEVX DCT object is destroyed
    without calling DRAIN DCT before, (e.g. under cleanup flow), need to
    manage its creation and destruction via mlx5 core.
    
    In that case the DRAIN DCT command will be called and only once that
    it will be completed the DESTROY DCT command will be called.
    Otherwise, the DESTROY DCT may fail and a hardware leak may occur.
    
    As of that change the DRAIN DCT command should not be exposed any more
    from DEVX, it's managed internally by the driver to work as expected by
    the device specification.
    
    issue: 1698643
    Change-Id: Ibd80a75ee350e0ee1ef7d8a42cfdb30ce4b8c83d
    Signed-off-by: Yishai Hadas <yishaih@mellanox.com>

--- drivers/infiniband/hw/mlx5/qp.c
+++ drivers/infiniband/hw/mlx5/qp.c
@@ -4202,6 +4202,7 @@ int mlx5_ib_modify_dct(struct ib_qp *ibqp, struct ib_qp_attr *attr,
 
 	} else if (cur_state == IB_QPS_INIT && new_state == IB_QPS_RTR) {
 		struct mlx5_ib_modify_qp_resp resp = {};
+		u32 out[MLX5_ST_SZ_DW(create_dct_out)] = {0};
 		u32 min_resp_len = offsetof(typeof(resp), dctn) +
 				   sizeof(resp.dctn);
 		u8 tclass = attr->ah_attr.grh.traffic_class;
@@ -4236,7 +4237,8 @@ int mlx5_ib_modify_dct(struct ib_qp *ibqp, struct ib_qp_attr *attr,
 		MLX5_SET(dctc, dctc, hop_limit, tmp_hop_limit);
 
 		err = mlx5_core_create_dct(dev->mdev, &qp->dct.mdct, qp->dct.in,
-					   MLX5_ST_SZ_BYTES(create_dct_in));
+					   MLX5_ST_SZ_BYTES(create_dct_in), out,
+					   sizeof(out));
 		if (err)
 			return err;
 		if (udata) {
