60ab6255bab830d92d107e81fb4e4765b58edf0a
Huy Nguyen <huyn@mellanox.com>
2019-03-19 13:59:47 -0500

    Revert "net/mlx5: Fix issue when deleting VEPA flow table"
    This reverts commit 09ce6e3971002b6b0dc85a2e69e2bf97f9e2ae4e.
    
    Issue: 1563741
    Change-Id: I57f12527fe33de81d1337dee5f60c8da25910301
    Signed-off-by: Huy Nguyen <huyn@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -402,9 +402,6 @@ out:
 
 static void esw_destroy_legacy_fdb_table(struct mlx5_eswitch *esw)
 {
-	struct mlx5_vport *evport;
-	int i;
-
 	esw_debug(esw->dev, "Destroy FDB Table\n");
 	if (esw->fdb_table.legacy.fdb) {
 		if (esw->fdb_table.legacy.promisc_grp)
@@ -422,13 +419,6 @@ static void esw_destroy_legacy_fdb_table(struct mlx5_eswitch *esw)
 	}
 
 	if (esw->fdb_table.legacy.vepa_fdb) {
-		for (i = 0; i < esw->total_vports; i++) {
-			evport = &esw->vports[i];
-			if (!evport->vepa_rule)
-				continue;
-			mlx5_del_flow_rules(evport->vepa_rule);
-			evport->vepa_rule = NULL;
-		}
 		mlx5_destroy_flow_table(esw->fdb_table.legacy.vepa_fdb);
 		esw->fdb_table.legacy.vepa_fdb = NULL;
 	}
@@ -2700,8 +2690,7 @@ int __mlx5_eswitch_set_vport_vepa_locked(struct mlx5_eswitch *esw,
 	void *misc;
 
 	if (!setting) {
-		if (evport->vepa_rule)
-			mlx5_del_flow_rules(evport->vepa_rule);
+		mlx5_del_flow_rules(evport->vepa_rule);
 		evport->vepa_rule = NULL;
 		return 0;
 	}
