d4b635f98296ee82d91cf53d08174f7ae536c0a7
Chris Mi <chrism@mellanox.com>
2019-03-22 01:15:50 -0400

    net/mlx5e: Protect encap hash table with lock and rcu
    To removed dependency on rtnl lock, protect encap hash table from
    concurrent modifications with spinlock. Change all operations on hash table
    to their rcu counterparts to allow hash table lookups without taking
    the lock.
    
    Issue: 1715919
    Change-Id: I89a2ad59fe90518a7983d04c5206b0ee314a7516
    Signed-off-by: Vlad Buslov <vladbu@mellanox.com>
    Reviewed-by: Jianbo Liu <jianbol@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.h
@@ -192,6 +192,7 @@ struct mlx5_esw_offload {
 	struct mlx5_flow_table *ft_offloads;
 	struct mlx5_flow_group *vport_rx_group;
 	struct mlx5_eswitch_rep *vport_reps;
+	spinlock_t encap_tbl_lock; /* protects encap_tbl */
 	struct list_head peer_flows;
 	struct mutex peer_mutex;
 	DECLARE_HASHTABLE(encap_tbl, 8);
