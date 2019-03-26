ce47231395231f39240833095177a7454b3c4d87
Chris Mi <chrism@mellanox.com>
2019-03-25 23:01:05 -0400

    gen-compat-config.sh: Fix pedit compile failure on JD kernel
    JD kernel(3.10.0-693.21.3) is based on centos 7.4, but it has a built-in
    act_pedit module. So don't set CONFIG_COMPAT_TCF_PEDIT_MOD on it.
    
    Issue: 1715919
    Change-Id: Idb4db3220632cc2e4e519c96a98475fcd8f101ae
    Fixes: a4dfc41b49387 ("net/sched: Support compat pedit action for centos 7.x")
    Signed-off-by: Chris Mi <chrism@mellanox.com>

--- ofed_scripts/gen-compat-config.sh
+++ ofed_scripts/gen-compat-config.sh
@@ -195,7 +195,9 @@ if [[ ! -z ${RHEL7_2} ]]; then
 	set_config CONFIG_COMPAT_TCF_VLAN_MOD m
 fi
 
-if [[ ${RHEL_MAJOR} -eq "7" && ${RHEL_MINOR} -le "4" ]]; then
+RHEL7_4_JD=$(echo ${KVERSION} | grep 3.10.0-693.21.3)
+
+if [[ ${RHEL_MAJOR} -eq "7" && ${RHEL_MINOR} -le "4" && ! $RHEL7_4_JD ]]; then
 	set_config CONFIG_COMPAT_TCF_PEDIT_MOD m
 fi
 
