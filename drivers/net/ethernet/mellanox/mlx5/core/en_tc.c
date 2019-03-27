d385b781013b1d1dab4c6a1d8de92bf0d919af7c
Chris Mi <chrism@mellanox.com>
2019-03-26 23:10:00 -0400

    net/mlx5e: define list_next_or_null_rcu
    Issue: 1715919
    Change-Id: I85885122ecd31ec46e4a7b24f25d2a39a1f70191
    Signed-off-by: Chris Mi <chrism@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_tc.c
@@ -1374,6 +1374,29 @@ mlx5e_get_next_encap_flow(struct mlx5e_encap_entry *e,
 	return found ? next : NULL;
 }
 
+#ifndef list_next_or_null_rcu
+/**
+ * list_next_or_null_rcu - get the first element from a list
+ * @head:       the head for the list.
+ * @ptr:        the list head to take the next element from.
+ * @type:       the type of the struct this is embedded in.
+ * @member:     the name of the list_head within the struct.
+ *
+ * Note that if the ptr is at the end of the list, NULL is returned.
+ *
+ * This primitive may safely run concurrently with the _rcu list-mutation
+ * primitives such as list_add_rcu() as long as it's guarded by rcu_read_lock().
+ */
+#define list_next_or_null_rcu(head, ptr, type, member) \
+({ \
+	struct list_head *__head = (head); \
+	struct list_head *__ptr = (ptr); \
+	struct list_head *__next = READ_ONCE(__ptr->next); \
+	likely(__next != __head) ? list_entry_rcu(__next, type, \
+						  member) : NULL; \
+})
+#endif
+
 static struct mlx5e_encap_entry *
 mlx5e_get_next_valid_encap(struct mlx5e_neigh_hash_entry *nhe,
 			   struct mlx5e_encap_entry *e)
