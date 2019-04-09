b9d27e8fa0ecfdd4d9279242b46645d0c178d387
Huy Nguyen <huyn@mellanox.com>
2019-04-09 11:13:05 -0500

    Revert "IB/core: Fix address overflow check in ib_umem_get"
    This reverts commit 0927e350811ebee40989ac854016e4976cc58cea.
    
    Since CAPI umem use zero length, we don't need this workaround.
    
    Issue: 1730475
    Change-Id: Ie53f82a984a08c8e63047f67f3737602a9046040
    Signed-off-by: Huy Nguyen <huyn@mellanox.com>

--- drivers/infiniband/core/umem.c
+++ drivers/infiniband/core/umem.c
@@ -205,7 +205,7 @@ struct ib_umem *ib_umem_get(struct ib_ucontext *context, unsigned long addr,
 	 * region causes an integer overflow, return error.
 	 */
 	if (((addr + size) < addr) ||
-	    (addr && PAGE_ALIGN(addr + size) < (addr + size))) {
+	    PAGE_ALIGN(addr + size) < (addr + size)) {
 		pr_err("%s: integer overflow, size=%zu\n", __func__, size);
  		return ERR_PTR(-EINVAL);
 	}
