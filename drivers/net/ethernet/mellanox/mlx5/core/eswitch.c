449fd4e341f9623cf6a28263baf77c74d6265826
Bodong Wang <bodong@mellanox.com>
2019-04-24 08:05:10 -0500

    net/mlx5: E-Switch, Enable correct number of VFs in legacy mode
    The number of vport enabled in legacy mode is not updated, which
    causes no vport was enabled at all.
    
    Update the number of VF vport in legacy as well.
    
    Issue: 1750714
    Change-Id: I885297be48056f40d531d4fd1cd47eb0058e85d6
    Fixes: 009061f0f3e4 ("net/mlx5: E-Switch, Handle host VF vports for BlueField")
    Signed-off-by: Bodong Wang <bodong@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
+++ drivers/net/ethernet/mellanox/mlx5/core/eswitch.c
@@ -2166,7 +2166,7 @@ void mlx5_eswitch_disable_vport(struct mlx5_eswitch *esw, u16 vport_num)
 
 int mlx5_eswitch_enable_sriov(struct mlx5_eswitch *esw, int nvfs, int mode)
 {
-	int vf_nvports = 0, total_nvports = 0;
+	int vf_nvports = nvfs, total_nvports = 0;
 	struct mlx5_vport *vport;
 	int err;
 	int i, enabled_events;
@@ -2192,7 +2192,6 @@ int mlx5_eswitch_enable_sriov(struct mlx5_eswitch *esw, int nvfs, int mode)
 				return err;
 			total_nvports = esw->total_vports;
 		} else {
-			vf_nvports = nvfs;
 			total_nvports = nvfs + MLX5_SPECIAL_VPORTS(esw->dev);
 		}
 	}
