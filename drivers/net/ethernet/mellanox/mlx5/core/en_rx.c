b6f3e3051acee1400473fcb2770909611f23c754
Shun Hao <shunh@mellanox.com>
2019-12-24 03:28:44 +0200

    Workaround to avoid crash when restarting OVS
    Signed-off-by: Shun Hao <shunh@mellanox.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
+++ drivers/net/ethernet/mellanox/mlx5/core/en_rx.c
@@ -1100,11 +1100,10 @@ static inline int mlx5e_build_rx_skb(struct mlx5_cqe64 *cqe,
 	u8 l4_hdr_type;
 #endif
 	u32 flow_tag = (be32_to_cpu(cqe->sop_drop_qpn) & MLX5E_TC_FLOW_ID_MASK);
-
 	if (flow_tag != (u32)MLX5E_DECAP_TABLE_MISS_TAG) {
 		decap_match = &priv->decap_match_table->data[flow_tag];
 		netdev = decap_match->vxlan_device;
-		if (!netdev) {
+		if (!netdev || netdev->reg_state != NETREG_REGISTERED) {
 			kfree_skb(skb);
 			return -ENODEV;
 		}
